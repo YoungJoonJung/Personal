d9de05bdff017a3e95e4c186002da9e82478c4d6
YoungJoonJung <41659661+YoungJoonJung@users.noreply.github.com>
2026-05-09 03:56:24 +0900

    Add files via upload

---
+++ ETF_Stock_P/MyDCIRP_DATA.js
@@ -0,0 +1,38 @@
+window.ETF_DATA = {
+  "DC": [
+    { name: "ACE AI반도체TOP3+", code: "469150" },
+    { name: "ACE KRX금현물", code: "411060" },
+    { name: "ACE 미국배당다우존스", code: "402970" },
+    { name: "ACE 미국빅테크TOP7 Plus", code: "465580" },
+    { name: "ACE 엔비디아채권혼합", code: "448540" },
+    { name: "KODEX 미국반도체", code: "390390" },
+    { name: "KODEX 삼성전자채권혼합", code: "448330" },
+    { name: "KODEX 헬스케어", code: "266420" },
+    { name: "PLUS K방산", code: "449450" },
+    { name: "PLUS 미국달러SOFR금리액티브(합성)", code: "456200" },
+    { name: "TIGER 200 중공업", code: "139230" },
+    { name: "TIGER 구리실물", code: "160580" },
+    { name: "TIGER 미국테크TOP10 INDXX", code: "381170" },
+    { name: "TIGER 미국필라델피아반도체나스닥", code: "381180" }
+  ],
+
+  "IRP": [
+    { name: "KODEX 200미국채혼합", code: "284430" },
+    { name: "KODEX AI반도체핵심장비", code: "471990" },
+    { name: "KODEX TRF3070", code: "329650" },
+    { name: "KODEX 미국S&P500에너지(합성)", code: "218420" },
+    { name: "KODEX 미국빅테크10(H)", code: "314250" },
+    { name: "SOL 미국배당미국채혼합50", code: "490490" },
+    { name: "TIGER 글로벌혁신블루칩TOP10", code: "464930" },
+    { name: "TIGER 미국필라델피아반도체나스닥", code: "381180" },
+    { name: "TIGER 코리아배당다우존스", code: "0052D0" }
+  ],
+
+  "연금저축": [
+    { name: "KODEX 구리선물(H)", code: "138910" },
+    { name: "ACE 미국나스닥100", code: "367380" },
+    { name: "HANARO Fn조선해운", code: "441540" },
+    { name: "ACE AI반도체포커스", code: "469150" },
+    { name: "TIGER 미국테크TOP10채권혼합", code: "472170" }
+  ]
+};
